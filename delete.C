75a7c6f50d315a305835f9b56b9a43d3404f2df5
idofek <ido.ofek@gmail.com>
2025-12-14 13:18:23 -0600

    added delete code

--- delete.C
+++ delete.C
@@ -1,5 +1,6 @@
 #include "catalog.h"
 #include "query.h"
+#include "stdlib.h"
 
 
 /*
@@ -16,11 +17,51 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
-
-
-
+    Status status;
+    AttrDesc attrDesc;
+    int intValue;
+    float floatValue;
+    
+    HeapFileScan scanner(relation, status);
+    if (status != OK) { return status; }
+    
+    if (attrValue == NULL) {
+        status = scanner.startScan(0, 0, STRING, NULL, EQ);
+        if (status != OK) { return status; }
+    }
+    else {
+        status = attrCat->getInfo(relation, attrName, attrDesc);
+        if (status != OK) { return status; }
+        
+        char* filterPtr = NULL;
+        
+        if (type == INTEGER) {
+            intValue = atoi(attrValue);
+            filterPtr = (char*)&intValue;
+        }
+        else if (type == FLOAT) {
+            floatValue = atof(attrValue);
+            filterPtr = (char*)&floatValue;
+        }
+        else if (type == STRING) {
+            filterPtr = (char*)attrValue;
+        }
+        
+        status = scanner.startScan(attrDesc.attrOffset,
+                                   attrDesc.attrLen,
+                                   (Datatype)attrDesc.attrType,
+                                   filterPtr,
+                                   op);
+        if (status != OK) { return status; }
+    }
+    
+    RID rid;
+    while (scanner.scanNext(rid) == OK) {
+        status = scanner.deleteRecord();
+        if (status != OK) { return status; }
+    }
+    
+    return OK;
 }
 
 
